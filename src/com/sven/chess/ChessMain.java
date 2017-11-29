73eef526e206087194eaf653510e417faa4407b5
Kwan <kwan@bmj.com>
2017-11-29 11:27:30 +0000

    handle symmetry permutation

--- src/com/sven/chess/ChessMain.java
+++ src/com/sven/chess/ChessMain.java
@@ -22,30 +22,71 @@ public class ChessMain
 
         long curr = System.currentTimeMillis();
         boolean isDebugEnabled = false;
-        
 
-//        ChessBoard chessBoard =
-//                new ChessBoard(3, 3,
-//                        Arrays.asList(King.INSTANCE, King.INSTANCE, Rook.INSTANCE));
+        // ChessBoard chessBoard =
+        // new ChessBoard(3, 3,
+        // Arrays.asList(King.INSTANCE, King.INSTANCE, Rook.INSTANCE));
 
-//        ChessBoard chessBoard =
-//                new ChessBoard(4, 4,
-//                        Arrays.asList(Rook.INSTANCE, Rook.INSTANCE, Knight.INSTANCE,
-//                                Knight.INSTANCE, Knight.INSTANCE, Knight.INSTANCE));
-        
+        // ChessBoard chessBoard =
+        // new ChessBoard(4, 4,
+        // Arrays.asList(Rook.INSTANCE, Rook.INSTANCE, Knight.INSTANCE,
+        // Knight.INSTANCE, Knight.INSTANCE, Knight.INSTANCE));
+
         ChessBoard chessBoard =
                 new ChessBoard(7, 7,
                         Arrays.asList(King.INSTANCE, King.INSTANCE, Queen.INSTANCE,
-                                Queen.INSTANCE, Bishop.INSTANCE, Bishop.INSTANCE, Knight.INSTANCE));
+                                Queen.INSTANCE, Bishop.INSTANCE, Bishop.INSTANCE,
+                                Knight.INSTANCE));
 
         List<List<Piece>> permutations = new ArrayList<>();
         getPermutations(permutations, new ArrayList<Piece>(), chessBoard.getPieces());
 
-        System.out.println(String.format("found %s permutations", permutations.size()));
+        List<String> subSymmetry = new ArrayList<>();
+        List<String> subNoSymmetry = new ArrayList<>();
+
+        List<String> ps =
+                permutations.stream().map(s -> s.stream().map(t -> t.getName()).collect(
+                        Collectors.joining())).sorted().collect(Collectors.toList());
+        ;
+
+        ps.stream().forEach(s -> {
+
+            String rs = new StringBuffer(s).reverse().toString();
+            if (!subSymmetry.contains(rs) && !subSymmetry.contains(s)
+                    && !subNoSymmetry.contains(rs) && !subNoSymmetry.contains(s))
+            {
+                if (rs.equalsIgnoreCase(s))
+                {
+                    subSymmetry.add(s);
+                }
+                else
+                {
+                    subNoSymmetry.add(s);
+                }
+
+            }
+        });
+
+        List<List<Piece>> subSymmetryPermutations = permutations.stream().filter(s -> subSymmetry.contains(
+                s.stream().map(j -> j.getName()).collect(Collectors.joining()))).collect(
+                        Collectors.toList());
+        
+        List<List<Piece>> subNoSymmetryPermutations = permutations.stream().filter(s -> subNoSymmetry.contains(
+                s.stream().map(j -> j.getName()).collect(Collectors.joining()))).collect(
+                        Collectors.toList());
+        System.out.println("sub permutations:" + subNoSymmetryPermutations.size() + subSymmetryPermutations.size());
+        
         if (isDebugEnabled)
         {
 
-            for (List<Piece> permutation : permutations)
+            for (List<Piece> permutation : subNoSymmetryPermutations)
+            {
+                String str = permutation.stream().map(s -> s.getName()).collect(
+                        Collectors.joining());
+                System.out.println(str);
+            }
+            
+            for (List<Piece> permutation : subSymmetryPermutations)
             {
                 String str = permutation.stream().map(s -> s.getName()).collect(
                         Collectors.joining());
@@ -54,23 +95,67 @@ public class ChessMain
         }
 
         // place piece to board
-        int total = 0;
+        int totalSymmetry = 0;
+        int totalNoSymmetry = 0;
         int index = 0;
-        for (List<Piece> permutation : permutations)
+        for (List<Piece> permutation : subSymmetryPermutations)
         {
-            System.out.println(String.format("permutation[%s] ...", index++));
+            System.out.println(String.format("subSymmetry[%s] ...", index++));
             ChessLayout layout = new ChessLayout(chessBoard, permutation);
             List<Map<Integer, Piece>> result = layout.getAllResult();
-            if (isDebugEnabled) {
-            result.forEach(s -> {
-                layout.print(s);
-                System.out.println("");
-                System.out.println("------------------------");
-            });
+            if (isDebugEnabled)
+            {
+                result.forEach(s -> {
+                    layout.print(s);
+                    System.out.println("");
+                    System.out.println("------------------------");
+                });
             }
-            total += result.size();
+            totalSymmetry += result.size();
         }
-        System.out.println(String.format("total: %s in %s secs", total, (System.currentTimeMillis() - curr) / 1000));
+        
+        for (List<Piece> permutation : subNoSymmetryPermutations)
+        {
+            System.out.println(String.format("subNoSymmetry[%s] ...", index++));
+            ChessLayout layout = new ChessLayout(chessBoard, permutation);
+            List<Map<Integer, Piece>> result = layout.getAllResult();
+            if (isDebugEnabled)
+            {
+                result.forEach(s -> {
+                    layout.print(s);
+                    System.out.println("");
+                    System.out.println("------------------------");
+                });
+            }
+            totalNoSymmetry += result.size();
+        }
+        System.out.println(String.format("total: %s in %s secs", totalSymmetry + totalNoSymmetry * 2,
+                (System.currentTimeMillis() - curr) / 1000));
+    }
+
+    public static List<List<Piece>> filter(final List<List<Piece>> permutations)
+    {
+
+        List<String> ps =
+                permutations.stream().map(s -> s.stream().map(t -> t.getName()).collect(
+                        Collectors.joining())).sorted().collect(Collectors.toList());
+        ;
+
+        List<String> rps = new ArrayList<String>();
+        ps.stream().forEach(s -> {
+
+            String rs = new StringBuffer(s).reverse().toString();
+            if (!rps.contains(rs) && !rps.contains(s))
+            {
+                rps.add(s);
+            }
+        });
+
+        List<List<Piece>> sub = permutations.stream().filter(s -> rps.contains(
+                s.stream().map(j -> j.getName()).collect(Collectors.joining()))).collect(
+                        Collectors.toList());
+        System.out.println("sub permutations:" + sub.size());
+        return sub;
     }
 
     public static <T> void getPermutations(final List<List<T>> permutations,
